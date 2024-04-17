9d2e6a290cc4c2afc0a65f867229e8b5ab4b7f0a
RKL715 <remykennyl715@gmail.com>
2024-04-17 17:19:03 +0200

    added   const last = data ? data.events[data.events.length - 1] : null; to get the last event

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -13,8 +13,10 @@ import Modal from "../../containers/Modal";
 import { useData } from "../../contexts/DataContext";
 
 const Page = () => {
-  const {last} = useData()
+  const {data} = useData()
+  const last = data ? data.events[data.events.length - 1] : null;
   console.log ("Value of 'last':", last)
+
   return <>
     <header>
       <Menu />
@@ -164,5 +166,6 @@ const Page = () => {
 export default Page;
 
 // 1. LINE   : PROP ImageSrc & Title issue (undefined) : Added {last&&()} to check if last is not null, then
+// added   const last = data ? data.events[data.events.length - 1] : null; to get the last event.
 // 2. LINE   : PROP ImageAlt missing : Added it
 // 3. LINE   : PROP label not right : Changed it to last?.type. Removed label="boom".
